c636d766305259a16bcb158aa08cac8282de6e2c
Alfredo Bautista <a.bautista@saltosystems.com>
2025-12-05 12:46:06 +0100

    inject bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:time_gem/calendar_bloc/calendar_bloc.dart';
+import 'package:calendar_view/calendar_view.dart';
 import 'package:time_gem/local_calendar/local_calendar_service.dart';
-import 'package:time_gem/welcome_screen.dart';
+import 'package:time_gem/task_bloc/task_bloc.dart';
+import 'package:time_gem/task_bloc/task_event.dart';
+import 'package:time_gem/ui/screens/welcome/welcome_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:time_gem/firebase_options.dart';
@@ -22,23 +25,33 @@ class MyApp extends StatelessWidget {
     final LocalCalendarService localCalendarService =
         LocalCalendarService(DatabaseHelper());
 
-    return BlocProvider(
-      create: (context) => CalendarBloc(
-        googleSignIn: GoogleSignIn(
-          scopes: [
-            'https://www.googleapis.com/auth/calendar.readonly',
-            'https://www.googleapis.com/auth/calendar.events',
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CalendarBloc(
+            googleSignIn: GoogleSignIn(
+              scopes: [
+                'https://www.googleapis.com/auth/calendar.readonly',
+                'https://www.googleapis.com/auth/calendar.events',
+              ],
+            ),
+            localCalendarService: localCalendarService,
+          ),
         ),
-        localCalendarService: localCalendarService,
-      ),
-      child: MaterialApp(
-        title: 'time_gem',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+        BlocProvider(
+          create: (context) => TaskBloc()..add(LoadTasks()),
         ),
-        home: const WelcomeScreen(),
+      ],
+      child: CalendarControllerProvider(
+        controller: EventController(),
+        child: MaterialApp(
+          title: 'time_gem',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const WelcomeScreen(),
+        ),
       ),
     );
   }
