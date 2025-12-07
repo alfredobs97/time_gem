56319bbb2661855c1e3132abf4678373985f4cf2
Alfredo Bautista <a.bautista@saltosystems.com>
2025-12-07 23:41:02 +0100

    splash screen

---
+++ lib/ui/screens/splash/splash_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:time_gem/session_bloc/session_bloc.dart';
+import 'package:time_gem/session_bloc/session_state.dart';
+import 'package:time_gem/ui/screens/home/home_screen.dart';
+import 'package:time_gem/ui/screens/welcome/welcome_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SessionBloc, SessionState>(
+      listener: (context, state) {
+        switch (state) {
+          case Authenticated():
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+              );
+            });
+            break;
+          case Unauthenticated():
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (_) => const WelcomeScreen(initialPage: 3),
+                ),
+              );
+            });
+            break;
+          case OnboardingIncomplete():
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (_) => const WelcomeScreen()),
+              );
+            });
+            break;
+          default:
+            break;
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Time Gem',
+                style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+              ),
+              const SizedBox(height: 24),
+              const CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
