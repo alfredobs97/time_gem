67af952783cdbf417f1e66f40ff7d44da09c771f
Alfredo Bautista <a.bautista@saltosystems.com>
2025-12-17 19:55:10 +0100

    inject all dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:time_gem/calendar_bloc/calendar_bloc.dart';
 import 'package:calendar_view/calendar_view.dart';
-import 'package:time_gem/local_calendar/local_calendar_service.dart';
+import 'package:time_gem/data/services/local_calendar_service.dart';
 import 'package:time_gem/task_bloc/task_bloc.dart';
 import 'package:time_gem/task_bloc/task_event.dart';
-import 'package:time_gem/ui/screens/welcome/welcome_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:time_gem/firebase_options.dart';
+
+import 'package:time_gem/data/services/google_calendar_service.dart';
+import 'package:time_gem/data/services/task_service.dart';
+import 'package:time_gem/data/services/gemini_ai_service.dart';
+import 'package:time_gem/ui/theme/app_theme.dart';
+import 'package:time_gem/session_bloc/session_bloc.dart';
+import 'package:time_gem/session_bloc/session_event.dart';
+
+import 'package:time_gem/data/services/storage_service.dart';
+import 'package:time_gem/ui/screens/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,37 +31,68 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final LocalCalendarService localCalendarService =
-        LocalCalendarService(DatabaseHelper());
-
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => CalendarBloc(
-            googleSignIn: GoogleSignIn(
-              scopes: [
-                'https://www.googleapis.com/auth/calendar.readonly',
-                'https://www.googleapis.com/auth/calendar.events',
-              ],
+    return FutureBuilder<StorageService>(
+      future: StorageService.init(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return MaterialApp(
+            title: 'time_gem',
+            theme: AppTheme.lightTheme,
+            home: const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
             ),
-            localCalendarService: localCalendarService,
+          );
+        }
+
+        final storageService = snapshot.data!;
+
+        final localCalendarService = LocalCalendarService(DatabaseHelper());
+        final taskService = TaskService();
+
+        final googleCalendarRepository = GoogleCalendarService(
+          googleSignIn: GoogleSignIn(
+            scopes: [
+              'https://www.googleapis.com/auth/calendar.readonly',
+              'https://www.googleapis.com/auth/calendar.events',
+            ],
           ),
-        ),
-        BlocProvider(
-          create: (context) => TaskBloc()..add(LoadTasks()),
-        ),
-      ],
-      child: CalendarControllerProvider(
-        controller: EventController(),
-        child: MaterialApp(
-          title: 'time_gem',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
+        );
+
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => SessionBloc(
+                storageService: storageService,
+                googleCalendarService: googleCalendarRepository,
+              )..add(AppStarted()),
+            ),
+            BlocProvider(
+              create: (context) => CalendarBloc(
+                googleCalendarRepository: googleCalendarRepository,
+                localCalendarService: localCalendarService,
+                taskService: taskService,
+              ),
+            ),
+            BlocProvider(
+              create: (context) => TaskBloc(
+                taskService: taskService,
+                aiService: GeminiAIService(
+                  googleCalendarRepository: googleCalendarRepository,
+                ),
+                storageService: storageService,
+              )..add(LoadTasks()),
+            ),
+          ],
+          child: CalendarControllerProvider(
+            controller: EventController(),
+            child: MaterialApp(
+              title: 'time_gem',
+              theme: AppTheme.lightTheme,
+              home: const SplashScreen(),
+            ),
           ),
-          home: const WelcomeScreen(),
-        ),
-      ),
+        );
+      },
     );
   }
 }
